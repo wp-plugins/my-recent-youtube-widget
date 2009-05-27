d87d16d2b699e282a74b9d36076b5d0016585f8a
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-05-27 12:51:54 +0000

    Initial check-in
    git-svn-id: https://plugins.svn.wordpress.org/my-recent-youtube-widget/trunk@120799 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ MyRecentYT.php
@@ -0,0 +1,162 @@
+<?php
+
+/**
+ * Copyright (c) 2009 Dave Ross <dave@csixty4.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR 
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ **/
+
+class MyRecentYT
+{
+	/**
+	 * Initialize the plugin
+	 */
+	function init()
+	{
+		register_sidebar_widget('My Recent YouTube', array(__CLASS__, 'renderWidget'));
+		register_widget_control('My Recent YouTube', array(__CLASS__, 'renderControl'), 400, 300);
+	}
+	
+	/**
+	 * Retrieve's this plugin's options & sets defaults where needed
+	 *
+	 * @return Array variables, process with extract()
+	 */
+	function getOptions()
+	{
+		// TODO support multiple instances with different configs
+		$username = get_option('my-recent-yt_username');
+		if(!$username) $username = '';
+		$numVideos = get_option('my-recent-yt_num_videos');
+		if(!$numVideos) $numVideos = 1;
+		$width = get_option('my-recent-yt_width');
+		if(!$width) $width = 320;
+		$height = get_option('my-recent-yt_height');
+		if(!$height) $height = 240;
+		
+		return compact('username', 'numVideos', 'width', 'height');
+	}
+	
+	/**
+	 * Render the widget for display (like in a sidebar)
+	 */
+	function renderWidget()
+	{	
+		echo '<div class="my-recent-yt-widget">';
+		extract(self::getOptions());
+		$videoIDs = self::getVideoIDs($username, $numVideos);
+		foreach($videoIDs as $videoID)
+		{
+			echo self::getVideoEmbed($videoID, $width, $height);
+		}
+		echo '</div>';
+	}
+	
+	function renderControl($widget_args)
+	{
+		// TODO support multiple instances with different configs
+		static $updated = false;
+		if ( is_numeric($widget_args) )
+			$widget_args = array( 'number' => $widget_args );
+		$widget_args = wp_parse_args( $widget_args, array( 'number' => -1 ) );
+		extract( $widget_args, EXTR_SKIP );
+	
+		if ( !$updated && !empty($_POST['sidebar']))
+		{
+			$username = $_POST['my-recent-yt_username'];
+			self::setOption('my-recent-yt_username', $username);
+			$numVideos = $_POST['my-recent-yt_num_videos'];
+			self::setOption('my-recent-yt_num_videos', $numVideos);
+			$width = $_POST['my-recent-yt_width'];
+			self::setOption('my-recent-yt_width', $width);
+			$height = $_POST['my-recent-yt_height'];
+			self::setOption('my-recent-yt_height', $height);
+		}
+		else
+		{
+			extract(self::getOptions());	
+		}
+	
+		include('my-recent-yt-admin.php.tpl');
+	}
+	
+	/**
+	 * Sets a WordPress option. Adds or updates as necessary
+	 *
+	 * @param string $name
+	 * @param mixed $value
+	 */
+	function setOption($name, $value)
+	{
+		if(FALSE !== get_option($name))
+			update_option($name, $value);
+		else
+			add_option($name, $value);
+	}
+	
+	/**
+	 * Enter description here...
+	 *
+	 * @param unknown_type $username
+	 * @param unknown_type $numVideos
+	 * @return Array
+	 */
+	function getVideoIDs($username, $numVideos = 10)
+	{
+		$ids = array();
+		
+		$feedURL = "http://gdata.youtube.com/feeds/api/users/$username/uploads?v=2&max-results=$numVideos";	
+
+		$xml = simplexml_load_file(rawurlencode($feedURL));
+		if($xml)
+		{
+			foreach($xml->entry as $entry)
+			{
+				$id = $entry->id;
+				
+				$matches = array();
+				
+				preg_match("/video:([^,\\ ]*)/", $id, $matches);
+				$ids[] = $matches[1];
+			}
+		}
+
+		return $ids;
+	}
+	
+	/**
+	 * Builds the YouTube embed HTML for the given video
+	 *
+	 * @param string $videoID
+	 * @param integer $width
+	 * @param integer $height
+	 * @return string
+	 */
+	function getVideoEmbed($videoID, $width, $height)
+	{
+		$embed = <<<EMBED
+			<div class="my-recent-yt-video">
+				<object width="$width" height="$height">
+					<param name="movie" value="http://www.youtube.com/v/$videoID"></param>
+					<param name="allowFullScreen" value="true"></param>
+					<param name="allowscriptaccess" value="always"></param>
+					<embed src="http://www.youtube.com/v/$videoID" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="$width" height="$height"></embed>
+				</object>
+			</div>
+EMBED;
+		return $embed;
+	}
+}
+
+?>
---
+++ my-recent-yt-admin.php.tpl
@@ -0,0 +1,22 @@
+<!-- My Recent YT Control -->
+<p>
+<label for="my-recent-yt_username">YouTube Username
+<input class="widefat" type="text" id="my-recent-yt_username" name="my-recent-yt_username" value="<?php echo $username; ?>" /></label>
+</p>
+
+<p>
+<label for="my-recent-yt_num_videos"><span style="display:block;">Number of videos</span>
+<input class="fat" type="text" id="my-recent-yt_num_videos" name="my-recent-yt_num_videos" value="<?php echo $numVideos; ?>" size="3" maxlength="3" /></label>
+</p>
+
+<p>
+<label for="my-recent-yt_width"><span style="display:block;">Width</span>
+<input class="fat" type="text" id="my-recent-yt_width" name="my-recent-yt_width" value="<?php echo $width; ?>" size="4" maxlength="4" /></label>
+</p>
+
+<p>
+<label for="my-recent-yt_height"><span style="display:block;">Height</span>
+<input class="fat" type="text" id="my-recent-yt_height" name="my-recent-yt_height" value="<?php echo $height; ?>" size="4" maxlength="4" /></label>
+</p>
+
+<!-- END:My Recent YT Control -->
