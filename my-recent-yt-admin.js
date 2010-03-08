d35d85470fc0487a9bebfbb44a2c15f2a340f9d9
csixty4 <csixty4@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-08 02:49:14 +0000

    // Safari makes us show() the div before it can slideDown()
    git-svn-id: https://plugins.svn.wordpress.org/my-recent-youtube-widget/trunk@214779 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- my-recent-yt-admin.js
+++ my-recent-yt-admin.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(function() {
 	jQuery('.my-recent-yt-extoggle').click(function() {
 		jQuery(this).hide();
-		jQuery(this).siblings('div.my-recent-yt-expanel').slideDown();
+		// Safari makes us show() the div before it can slideDown()
+		jQuery(this).siblings('div.my-recent-yt-expanel').show().slideDown();
 	});
 });
